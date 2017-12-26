0c90cb1fadb208ea388fc395646134520e6ea97a
Caleb Morris <caleb.morris.g@gmail.com>
2017-12-25 21:39:50 -0700

    Mocha working in browser

---
+++ src/index.js
@@ -0,0 +1,11 @@
+var expect = chai.expect;
+
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      var out = React.createElement(Greeting);
+      expect(out).to.not.be.null;
+      expect(1).to.equal(2);
+    });
+  });
+});
